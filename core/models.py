250b075dbb9e771c94e895cb122e0679731b65c3
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-06-07 15:10:27 +0300

    "Added poster image field to Blog model and updated __str__ method. Added a div element to course.html template."

--- core/models.py
+++ core/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
+    poster = models.ImageField(upload_to='blog-posts-poster/', null=True)
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=255)
     description = models.TextField()
     posted_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title + " " + self.posted_date
-    
+        return self.title
     
