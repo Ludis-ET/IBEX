978248d781f618ea0c7414d377800d6ce9aa29ed
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-06-07 14:43:52 +0300

    "Updated BlogAdmin in admin.py to display title, posted_date, and author, added filters for author and posted_date, and search fields for title and description."

--- core/admin.py
+++ core/admin.py
@@ -4,4 +4,6 @@ from .models import *
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'posted_date', 'author']
+    list_filter = ['author', 'posted_date']
+    search_fields = ['title', 'description']
